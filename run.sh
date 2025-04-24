80ac5517d42a28fdf5be43dbed78a716015e90c2
Lililiiz <565790159@qq.com>
2025-04-24 15:26:23 +0800

    training for GCNC

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 export OMP_NUM_THREADS=1
-# export CUDA_VISIBLE_DEVICES=2,3,4,5,6,7
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5
 torchrun \
-  --nproc_per_node 8 \
+  --nproc_per_node 6 \
   --master_port 29550 \
-  train_class_GCM.py
+  train_seg_GCNC.py
